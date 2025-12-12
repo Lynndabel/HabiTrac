1bb1feac1b99b95cc2f257b7f4553f46a05b9e4e
thebabalola <t.babalolajoseph@gmail.com>
2025-12-12 03:46:41 +0100

    Add test for initial streak of 0 for new habit

--- contracts/habitrac/test/HabiTracStreak.test.js
+++ contracts/habitrac/test/HabiTracStreak.test.js
@@ -15,7 +15,21 @@ describe("HabiTrac - Streak Calculation Tests", function () {
   });
 
   describe("Streak Calculation", function () {
-    // Tests will be added incrementally
+    it("Should start with streak of 0 for new habit", async function () {
+      const tx = await habiTrac.connect(user1).createHabit("Test Habit", "Test Description");
+      const receipt = await tx.wait();
+      const event = receipt.logs.find(log => {
+        try {
+          return habiTrac.interface.parseLog(log).name === "HabitCreated";
+        } catch {
+          return false;
+        }
+      });
+      const habitId = habiTrac.interface.parseLog(event).args.habitId;
+      
+      const streak = await habiTrac.getHabitStreak(user1.address, habitId);
+      expect(streak).to.equal(0);
+    });
   });
 });
 
